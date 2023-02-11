404c65d7989799645cb7fe0693291461c1a90eee
SantiSvk <santisverko@gmail.com>
2023-02-11 04:12:03 -0300

    Generador ID

--- src/components/PatientInfo.jsx
+++ src/components/PatientInfo.jsx
@@ -1,21 +1,33 @@
-function PatientInfo() {
+function PatientInfo({ paciente }) {
 	return (
 		<div className="bg-white shadow-lg rounded-xl p-5 mt-5 ml-2">
 			<p className="font-bold uppercase mb-3 text-gray-700">
+				Mascota: {""}
+				<span className="font-normal normal-case">
+					{paciente.name}
+				</span>
+			</p>
+			<p className="font-bold uppercase mb-3 text-gray-700">
 				Propietario: {""}
-				<span className="font-normal normal-case">Perro</span>
+				<span className="font-normal normal-case">
+					{paciente.owner}
+				</span>
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Email: {""}
-				<span className="font-normal normal-case">Perro</span>
+				<span className="font-normal normal-case">{paciente.email}</span>
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Fecha: {""}
-				<span className="font-normal normal-case">Perro</span>
+				<span className="font-normal normal-case">{paciente.date}</span>
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Síntomas: {""}
-				<span className="font-normal normal-case">Perro</span>
+				<span className="font-normal normal-case">{paciente.sympthoms}</span>
+			</p>
+			<p className="font-bold uppercase mb-3 text-gray-700">
+				Identificador: {""}
+				<span className="font-normal normal-case">{paciente.id}</span>
 			</p>
 		</div>
 	);
