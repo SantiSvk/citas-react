ade9159e0759ee0e31e3eb8cd9b857cb2b5c2029
SantiSvk <santisverko@gmail.com>
2023-02-13 04:30:19 -0300

    add buttons

--- src/components/PatientInfo.jsx
+++ src/components/PatientInfo.jsx
@@ -3,9 +3,7 @@ function PatientInfo({ paciente }) {
 		<div className="bg-white shadow-lg rounded-xl p-5 mt-5 ml-2">
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Mascota: {""}
-				<span className="font-normal normal-case">
-					{paciente.name}
-				</span>
+				<span className="font-normal normal-case">{paciente.name}</span>
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Propietario: {""}
@@ -15,7 +13,9 @@ function PatientInfo({ paciente }) {
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Email: {""}
-				<span className="font-normal normal-case">{paciente.email}</span>
+				<span className="font-normal normal-case">
+					{paciente.email}
+				</span>
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Fecha: {""}
@@ -23,12 +23,23 @@ function PatientInfo({ paciente }) {
 			</p>
 			<p className="font-bold uppercase mb-3 text-gray-700">
 				Síntomas: {""}
-				<span className="font-normal normal-case">{paciente.sympthoms}</span>
+				<span className="font-normal normal-case">
+					{paciente.sympthoms}
+				</span>
 			</p>
-			<p className="font-bold uppercase mb-3 text-gray-700">
-				Identificador: {""}
-				<span className="font-normal normal-case">{paciente.id}</span>
-			</p>
+			<div className="flex">
+				<button
+					style={{flexGrow: 6}}
+					type="button"
+					className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 hover:shadow-md font-semibold">
+					Editar
+				</button>
+				<button
+					type="button"
+					className="flex-1 p-2 bg-red-800 text-white rounded-md hover:bg-red-900 hover:shadow-md mx-3 font-semibold">
+					Elminar
+				</button>
+			</div>
 		</div>
 	);
 }
